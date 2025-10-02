6234bab0f088891dc505c72e489d47e53dbc017e
229846987 <Trong-Don@users.noreply.github.com>
2025-10-01 21:28:00 -0400

    Completed lab 5

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,47 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    star = n * "*" + "\n"
+    space = "*" + ((n - 2) * " ") + "*" + "\n"
+    result = ""
+
+    if n == 1:
+        result = star
+
+    else:
+        result += star + (n - 2)*(space) + star
+        
+    return result.strip() # strip() removes all spaces from both ends, while rstrip() removes trailing spaces (so only at the end)
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    number = ""
+    line = ""
+    count = 1
+
+    while count <= n:
+        line += str(count)
+        number += line + "\n"
+        count += 1
+    # for i in range(1, n + 1):
+    #     line += str(i)
+    #     number += line + "\n"
+
+    return number.rstrip()
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    count = 0
+    sum = 0
+
+    while count <= n:
+        sum += count
+        count += 1
+
+    return sum
 
 # Example for n = 4:
 #    *
@@ -24,4 +53,13 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    star = ""
+    space = ""
+    pyramid = ""
+
+    for j in range (1, n + 1):
+        space = ((n - j) * " ")
+        star = (((2 * j) - 1) * "*")
+        pyramid += space + star + "\n"
+
+    return pyramid.rstrip()
